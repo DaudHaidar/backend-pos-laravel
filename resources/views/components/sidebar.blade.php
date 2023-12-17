da83c2880bfaca1fdbf6e480fa4e1f8890ef42ce
DaudHaidar <abdillahdaudhaidar@example.com>
2023-12-17 15:39:07 +0700

    feat : import template 'stisla'

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,13 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.html">Stisla</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">St</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Dashboard </li>
+        </ul>
+    </aside>
+</div>
